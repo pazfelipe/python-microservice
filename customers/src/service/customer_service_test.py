23cf42c8eec299ac3c1213eae9194eccb3b1d97f
Felipe Paz <xxfelipepaz@gmail.com>
2024-02-09 22:30:48 -0300

    test: refactor function to test errors separately

--- customers/src/service/customer_service_test.py
+++ customers/src/service/customer_service_test.py
@@ -6,21 +6,25 @@ class CustomerServiceTest(unittest.TestCase):
 
     def setUp(self) -> None:
         self.customer = CustomerService()
+        self.mock_customer = {"name": "John Doe", "email": "test@gmail.com"}
         return super().setUp()
 
     def test_get_customer(self):
         pass
 
+    def test_create_customer_raise_error_if_name_is_missing(self):
+        with self.assertRaises(ValueError):
+            self.mock_customer.pop("name")
+            self.customer.create_customer(self.mock_customer)
+
+    def test_create_customer_raise_error_if_email_is_missing(self):
+        with self.assertRaises(ValueError):
+            self.mock_customer.pop("email")
+            self.customer.create_customer(self.mock_customer)
+
     def test_create_customer(self):
-        with self.assertRaises(ValueError):
-            self.customer.create_customer(customer={"name": "test"})
 
-        with self.assertRaises(ValueError):
-            self.customer.create_customer(customer={"name": "John Doe"})
-
-        response = self.customer.create_customer(
-            customer={"name": "John Doe", "email": "test@email.com"}
-        )
+        response = self.customer.create_customer(self.mock_customer)
         self.assertTrue("id" in response)
         self.assertEqual(response["id"], 1)
 
